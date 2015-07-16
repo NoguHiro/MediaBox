c6e16173c32479e65729d49270a0a78a6569c5cd
noguhiro <g007c1280@gmail.com>
2015-07-17 04:59:20 +0900

    update js

--- js/jquery.media.box.js
+++ js/jquery.media.box.js
@@ -3,30 +3,6 @@
 
   var pluginName = 'mediaBox';
 
-  var defaults = {
-    html: {
-      base: '<div class="mb-base">' +
-              '<div class="mb-contents">' +
-                '<span class="mb-close mb-decoration-close"><img src=":src"></span>' +
-                '<div class="mb-arrange"></div>' +
-              '</div>' +
-            '</div>',
-      video: '<video class="mb-video mb-close" autoplay src=":src" width=":width" height=":height"></video>',
-      image: '<img class="mb-image mb-close" src=":src" width=":width" height=":height" />'
-    },
-    elem: {
-      base: '.mb-base',
-      contents: '.mb-contents',
-      arrange: '.mb-arrange',
-      video: '.mb-video',
-      image: '.mb-image',
-      close: '.mb-close'
-    },
-    closeImage: 'close.png',
-    openSpeed: 500,
-    closeSpeed: 300
-  };
-
   /**
    * class MediaBox
    * @param {string} elementName
@@ -152,6 +128,33 @@
    */
   MediaBox.prototype._onCancel = function() {
     this._close();
+  };
+
+  /**
+   * Default params.
+   */
+  var defaults = {
+    html: {
+      base: '<div class="mb-base">' +
+      '<div class="mb-contents">' +
+      '<span class="mb-close mb-decoration-close"><img src=":src"></span>' +
+      '<div class="mb-arrange"></div>' +
+      '</div>' +
+      '</div>',
+      video: '<video class="mb-video mb-close" autoplay src=":src" width=":width" height=":height"></video>',
+      image: '<img class="mb-image mb-close" src=":src" width=":width" height=":height" />'
+    },
+    elem: {
+      base: '.mb-base',
+      contents: '.mb-contents',
+      arrange: '.mb-arrange',
+      video: '.mb-video',
+      image: '.mb-image',
+      close: '.mb-close'
+    },
+    closeImage: 'close.png',
+    openSpeed: 500,
+    closeSpeed: 300
   };
 
   $.fn[pluginName] = function(params) {
